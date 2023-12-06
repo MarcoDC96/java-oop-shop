998f4490dc6d8ce67eaa62de88144253d003559d
MarcoDC96 <marco.decicco@live.it>
2023-12-06 15:56:31 +0100

    Finish without bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,43 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Inserisci il numero di prodotti: ");
+        int numeroProdotti = Integer.parseInt(scan.nextLine());
+
+    Prodotto [] listaProdotti= new Prodotto[numeroProdotti];
+
+        for (int i = 0;  i< listaProdotti.length ; i++) {
+            System.out.print("Nome prodotto: ");
+            String nomeProdotto = scan.nextLine();
+            System.out.print("Descrivi il prodotto: ");
+            String descrizioneProdotto= scan.nextLine();
+            System.out.print("Costo prodotto: ");
+            double costoProdotto= Double.parseDouble(scan.nextLine());
+            System.out.print("Scrivi l'iva: ");
+            double costoIva = Double.parseDouble(scan.nextLine());
+
+            Prodotto prodotto = new Prodotto(nomeProdotto, descrizioneProdotto, costoProdotto, costoIva);
+            listaProdotti[i] = prodotto;
+        }
+
+        System.out.println("Prodotti: ");
+        for (int i = 0; i < listaProdotti.length; i++) {
+            System.out.println("Nome prodotto: " + listaProdotti[i].getNome());
+            System.out.println("Descrivi Prodotto: " + listaProdotti[i].getDescrizione());
+            System.out.println("Costo Prodotto: " + listaProdotti[i].getPrezzo());
+            System.out.println("Iva associata al prodotto: " + listaProdotti[i].getIva());
+            System.out.println("Prezzo con iva: " + listaProdotti[i].getPrezzoIva());
+            System.out.println("Codice prodotto: " + listaProdotti[i].getCodice());
+            System.out.println("Codice-Nome prodotto: " + listaProdotti[i].getCodiceNome());
+
+        }
+
+
+        scan.close();
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,74 @@
-package org.lessons.java.shop;public class Prodotto {
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+    //ATTRIBUTI
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+    //COSTRUTTORE
+    public Prodotto(String nome, String descrizione, double prezzo, double iva) {
+        this.codice = generaCodice();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    //GETTER - SETTER
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    //METODI
+
+    private int generaCodice(){
+        Random random = new Random();
+        return random.nextInt(1000, 10000);
+    }
+
+    public double getPrezzoIva() {
+        return (prezzo * iva) / 100;
+    }
+
+        public String getCodiceNome(){
+        return codice +"-"+ nome;
+    }
 }
